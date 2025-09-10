1ec20ce318a6f5aa90ac749a5abaeff936f6f493
Anower Hossen <140152829+Anowervaiya@users.noreply.github.com>
2025-09-10 19:49:01 +0600

    Update L2A2.sql

--- L2A2.sql
+++ L2A2.sql
@@ -1,13 +1,16 @@
-CREATE TABLE rangers (
-    ranger_id SERIAL PRIMARY KEY,
-    ranger_name TEXT NOT NULL,
-    region TEXT NOT NULL
-);
+create table rangers (
+  ranger_id serial primary key ,
+  name varchar(50) not null,
+  region varchar(50)
+)
 
-INSERT INTO rangers (ranger_id, ranger_name, region) VALUES
-(1, 'Alice Green', 'Northern Hills'),
-(2, 'Bob White', 'River Delta'),
-(3, 'Carol King', 'Mountain Range');
+
+insert into rangers (name, region)
+values ( 'Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+
 
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
@@ -17,11 +20,11 @@ CREATE TABLE species (
     conservation_status TEXT NOT NULL
 );
 
-INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
-(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
-(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
-(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
-(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+INSERT INTO species ( common_name, scientific_name, discovery_date, conservation_status) VALUES
+( 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+( 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+( 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+( 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
 
 
@@ -34,18 +37,16 @@ CREATE TABLE sightings (
     notes TEXT
 );
 
-INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
-(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
-(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
-(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
-(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
-
-
-SELECT * FROM rangers;
+INSERT INTO sightings ( species_id, ranger_id, location, sighting_time, notes) VALUES
+( 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+( 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+( 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+( 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+select * from sightings;
 
 -- problem 1
-INSERT INTO rangers (ranger_id, ranger_name, region) VALUES
-(4, 'Derek Fox', 'Coastal Plains');
+INSERT INTO rangers ( name, region) VALUES
+( 'Derek Fox', 'Coastal Plains');
 
 -- problem 2
 
@@ -57,23 +58,22 @@ SELECT * FROM sightings WHERE location LIKE '%Pass%';
 
 --problem 4 
 
-SELECT ranger_name as name , count(ranger_id) as total_sightings from rangers 
+SELECT name  , count(ranger_id) as total_sightings from rangers 
    JOIN sightings USING(ranger_id)
-   GROUP BY ranger_name;
+   GROUP BY name;
 
 --problem 5
 
 SELECT common_name FROM species
  left JOIN sightings USING(species_id)
  WHERE sighting_id is NULL 
- ; 
+ ;
 
 --  problem 6
 
-SELECT common_name, sighting_time, ranger_name FROM species
+SELECT common_name, sighting_time, name FROM species
   JOIN sightings USING(species_id)
-  JOIN rangers USING (ranger_id) ORDER BY sighting_time DESC LIMIT 2;
-
+  JOIN rangers USING (ranger_id) ORDER BY sighting_time ASC LIMIT 2;
 
 --problem 7
 
@@ -109,6 +109,3 @@ DELETE  from rangers
     FROM sightings
     WHERE sightings.ranger_id = rangers.ranger_id
     )
-
-
-SELECT * FROM rangers;
